581dfcfe7e0e33ef9891211eb8f250e723d04c30
jws2170 <mpart2077@gmail.com>
2024-03-09 18:14:05 +0900

    [ADD] Jump

--- Assets/GameProject/Scripts/Player.cs
+++ Assets/GameProject/Scripts/Player.cs
@@ -7,6 +7,12 @@ public class Player : MonoBehaviour
 {
     public float speed;
 
+    public float JumpPower;
+
+    private bool grounded;
+
+    private bool IsJump;
+
     float Horizontal;
 
     Rigidbody2D rb;
@@ -21,19 +27,25 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        
+        grounded = false;
+        IsJump = false;
     }
 
     private void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
 
-        
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            IsJump = true;
+        }
+
     }
 
     private void FixedUpdate()
     {
         Move();
+        Jump();
     }
 
     void Move()
@@ -41,4 +53,24 @@ public class Player : MonoBehaviour
         rb.velocity = new Vector2(Horizontal * speed, rb.velocity.y);
     }
 
+    void Jump()
+    {
+        if (IsJump)
+        {
+            rb.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+            IsJump = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            grounded = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        grounded = false;
+    }
 }
